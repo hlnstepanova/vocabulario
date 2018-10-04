7ba96fef880db94996b4f63573e54e511bb41d0d
hlnstepanova <hlnstepanova@yandex.ru>
2018-10-04 16:32:41 +0200

    Added level choice layout

---
+++ app/src/main/java/com/example/estepanova/vocablurybooster/LevelChoice.java
@@ -0,0 +1,13 @@
+package com.example.estepanova.vocablurybooster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LevelChoice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_choice);
+    }
+}
