c0d7af0f97036a800cdf4dfa39b218cc39c45954
hlnstepanova <hlnstepanova@yandex.ru>
2019-03-24 20:10:12 +0100

    Fix progress cells in topics choice table layout

--- app/src/main/java/com/example/estepanova/vocablurybooster/MainShow.java
+++ app/src/main/java/com/example/estepanova/vocablurybooster/MainShow.java
@@ -36,7 +36,7 @@ public class MainShow extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_dark);
+        setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
